4f170d7ed262b8edc848ffbbd95be9ba7a59ab11
JuanPabl07DP <juanpadazer15@gmail.com>
2025-02-17 20:37:03 -0500

    Modificados los servicios para usar los filtros

--- src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
+++ src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
@@ -1,11 +1,15 @@
 package edu.eci.arsw.blueprints.services;
 
+import edu.eci.arsw.blueprints.filters.BlueprintFilter;
 import edu.eci.arsw.blueprints.model.Blueprint;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+
+import java.util.HashSet;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,13 +22,23 @@ public class BlueprintsServices {
    
     @Autowired
     BlueprintsPersistence bpp;
+
+    @Autowired
+    @Qualifier("redundancyFilter")
+    BlueprintFilter bpFilter;
     
     public void addNewBlueprint(Blueprint bp) throws BlueprintPersistenceException {
         bpp.saveBlueprint(bp);
     }
     
     public Set<Blueprint> getAllBlueprints(){
-        return bpp.getAllBlueprints();
+        Set<Blueprint> allBlueprints = bpp.getAllBlueprints();
+        Set<Blueprint> filteredBlueprints = new HashSet<>();
+
+        for (Blueprint blueprint : allBlueprints) {
+            filteredBlueprints.add(bpFilter.filter(blueprint));
+        }
+        return filteredBlueprints;
     }
     
     /**
@@ -35,7 +49,8 @@ public class BlueprintsServices {
      * @throws BlueprintNotFoundException if there is no such blueprint
      */
     public Blueprint getBlueprint(String author,String name) throws BlueprintNotFoundException{
-        return bpp.getBlueprint(author,name);
+        Blueprint blueprint = bpp.getBlueprint(author, name);
+        return bpFilter.filter(blueprint);
     }
     
     /**
@@ -45,7 +60,9 @@ public class BlueprintsServices {
      * @throws BlueprintNotFoundException if the given author doesn't exist
      */
     public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException{
-        return bpp.getBlueprintsByAuthor(author);
+        Set<Blueprint> authorBlueprints = bpp.getBlueprintsByAuthor(author);
+        Set<Blueprint> filteredBlueprints = new HashSet<>();
+        return filteredBlueprints;
     }
     
 }
